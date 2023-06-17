2a95e4d7349e739e63b4359248f487c77f064a66
Yogesh More <yogesh.more2098@gmail.com>
2023-06-17 22:50:54 +0530

    Added: Welcome message.

---
+++ employeePayrollApp/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Welcome to employee payroll application." />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
